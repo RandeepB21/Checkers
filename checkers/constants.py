742d7f09cac17a7956de27614badec54ac5d3a4a
Randeep Bedi <59211675+RandeepB21@users.noreply.github.com>
2022-02-19 19:13:42 -0500

    Create constants.py

---
+++ checkers/constants.py
@@ -0,0 +1,9 @@
+WIDTH, HEIGHT = 700, 700 
+ROWS, COLS = 8, 8 
+SQUARE_SIZE = WIDTH // COLS 
+RED = (255, 0, 0) 
+WHITE = (255, 255, 255) 
+BLACK = (0, 0, 0) 
+BLUE = (0, 0, 255) 
+GREY = (128, 128, 128) 
+YELLOW = (255, 255, 0) 
